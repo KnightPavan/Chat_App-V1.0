36ca70be296b135b27dbfbfa8dc3e26b2cbe763a
KnightPavan <knightpavan75@gmail.com>
2023-04-07 10:05:39 +0530

    Update for chatroom
    Client and the server files have been update to support messaging

--- Client.py
+++ Client.py
@@ -1,7 +1,32 @@
 import socket
+import threading
 
 IP = "192.168.1.10"
 PORT = 1234
+
+#To listen to the messages from other users
+def msg_listner(client, myUserName):
+    while True:
+        msg = client.recv(10000).decode('utf-8')
+        userName = msg.split("-->")[0]
+        content = msg.split("-->")[1]
+        if (userName != myUserName):
+            print(f"[{userName}] {content}")
+
+#To send the username to the server
+def initial_msg(client):
+    userName = input("Enter UserName : ")
+    if userName != '':
+        client.sendall(userName.encode())
+    else:
+        print("UserName Invalid. Please enter the correct UserName")
+    return userName
+
+#To send the messages to the server
+def msg_send(client):
+    while True:
+        msg = input(">>> ")
+        client.sendall(msg.encode())
 
 def main():
     
@@ -9,8 +34,13 @@ def main():
     try:
         client.connect((IP, PORT))
         print("Connection established successfully")
+        userName = initial_msg(client)
+        
+        threading.Thread(target=msg_listner, args=(client, userName)).start()
+        threading.Thread(target=msg_send, args=(client, )).start()
     except:
         print("Error")
+    
 
 if __name__ == "__main__":
     main()
--- Server.py
+++ Server.py
@@ -1,46 +1,48 @@
 import socket
 import threading
-IP = "192.168.1.10"
-PORT = 1234
+
+IP = "192.168.1.10" #Change it to your ip address
+PORT = 1234 #Change if not available
 LIST_LIMIT = 5
 CLIENT_LIST = []
 
-def listen_message(userName, client):
-
-    while True:
-        message = client.recv(2048).decode('utf-8')
-        final_msg = f'{userName} --> {message}'
-        send_message_to_all(final_msg)
-
+#Send the message received from client to all other users
 def send_message_to_all(msg):
-
+    print(msg)
     for user in CLIENT_LIST:
         client = user[1]
         client.sendall(msg.encode())
 
+#Listening to messages from client
+def listen_message(userName, client):
+
+    while True:
+        message = client.recv(10000).decode('utf-8')
+        final_msg = f'{userName}-->{message}'
+        send_message_to_all(final_msg)
+
+#To get username
 def client_handler(client):
 
     while True:
-        userName = client.recv(2048).decode('utf-8')
+        userName = client.recv(10000).decode('utf-8')
         if userName != '':
             print("Username Done!")
             CLIENT_LIST.append((userName, client))
             break
-        else:
-            print("Username not found")
-    threading.Thread(target=listen_message, args=(userName, client))
+
+    threading.Thread(target=listen_message, args=(userName, client)).start()
 
 def main():
-
+    #Creating socket object with IPV4 and TCP protocol
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server.bind((IP,PORT))
+        server.bind((IP,PORT)) 
         print("Bind Successful")
         server.listen(LIST_LIMIT)
-
+        #To Handle multiple clients
         while True:
-            client, address = server.accept()
-            
+            client, address = server.accept()  
             print(f'Successfully connected to {address[0]} : {address[1]}')
             threading.Thread(target=client_handler, args=(client, )).start()
 
