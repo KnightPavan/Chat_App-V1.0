a1bf581b88046ee9c422a23ad62ae19b8fcea2db
KnightPavan <knightpavan75@gmail.com>
2023-04-07 21:42:20 +0530

    DM Functionality Added
    Direct messaging is now included and also added user list display in client to either dm users or broadcast to all

--- Client.py
+++ Client.py
@@ -1,17 +1,21 @@
 import socket
 import threading
 
-IP = "192.168.1.10"
+IP = "192.168.1.10" #Change ip and port number to that of the server
 PORT = 1234
 
 #To listen to the messages from other users
 def msg_listner(client, myUserName):
     while True:
-        msg = client.recv(10000).decode('utf-8')
+        msg = client.recv(50000).decode('utf-8')
         userName = msg.split("-->")[0]
         content = msg.split("-->")[1]
-        if (userName != myUserName):
-            print(f"[{userName}] {content}")
+        Name = myUserName
+        if userName[:4]!= "[Dm]":
+            Name = f"[{myUserName}]"
+
+        if (userName != Name):
+            print(f"{userName} {content}")
 
 #To send the username to the server
 def initial_msg(client):
@@ -28,18 +32,36 @@ def msg_send(client):
         msg = input(">>> ")
         client.sendall(msg.encode())
 
+#Handles Options
+def msg_routing(client, userName):
+    
+    while True:
+        data = client.recv(50000).decode('utf-8')
+        list = data.split("+")
+        print(" 0. Reload Clients")
+        print("-1. Broadcast To All")
+        for i in range(1, len(list)):
+            print(f" {i}. {list[i-1]}")
+        
+        option = str(input("Enter your choice : "))
+        client.sendall(option.encode())
+        if int(option) != 0:
+            break
+    
+
 def main():
     
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client.connect((IP, PORT))
-        print("Connection established successfully")
-        userName = initial_msg(client)
-        
-        threading.Thread(target=msg_listner, args=(client, userName)).start()
-        threading.Thread(target=msg_send, args=(client, )).start()
-    except:
-        print("Error")
+    # try:
+    client.connect((IP, PORT))
+    print("Connection established successfully")
+    userName = initial_msg(client)
+    
+    msg_routing(client, userName)
+    threading.Thread(target=msg_listner, args=(client, userName)).start()
+    threading.Thread(target=msg_send, args=(client, )).start()
+    # except :
+    #     print("Unsuccessfull")
     
 
 if __name__ == "__main__":
--- Server.py
+++ Server.py
@@ -10,28 +10,59 @@ CLIENT_LIST = []
 def send_message_to_all(msg):
     print(msg)
     for user in CLIENT_LIST:
-        client = user[1]
-        client.sendall(msg.encode())
+        try:
+            client = user[1]
+            client.sendall(msg.encode())
+        except:
+            pass
+
+#Handles Peer to Peer Dm
+def private_msg(client, msg, userName):
+    final_msg = f'[Dm][{userName}]-->{msg}'
+    print(final_msg)
+    client.sendall(final_msg.encode())
+
 
 #Listening to messages from client
-def listen_message(userName, client):
+def listen_message(userName, client, option):
 
     while True:
-        message = client.recv(10000).decode('utf-8')
-        final_msg = f'{userName}-->{message}'
-        send_message_to_all(final_msg)
+        message = client.recv(50000).decode('utf-8')
+        final_msg = f'[{userName}]-->{message}'
+        if option == -1:
+            send_message_to_all(final_msg)
+        else:
+            private_msg(CLIENT_LIST[option-1][1], message, userName)
+
+#Repeadedly Sends Choice
+def option_handler(client):
+    while True:
+        option = int(client.recv(50000).decode('utf-8'))
+        if option==0:
+            send_choice(client)
+        else:
+            return option
+    
+#Sending the Clients connected
+def send_choice(client):
+    data = ""
+    for user in CLIENT_LIST:
+        data += f"{user[0]}+"
+    client.sendall(data.encode())
 
 #To get username
 def client_handler(client):
 
     while True:
-        userName = client.recv(10000).decode('utf-8')
+        userName = client.recv(50000).decode('utf-8')
         if userName != '':
             print("Username Done!")
             CLIENT_LIST.append((userName, client))
             break
-
-    threading.Thread(target=listen_message, args=(userName, client)).start()
+    send_choice(client)
+    option = option_handler(client)
+    threading.Thread(target=listen_message, args=(userName, client, option)).start()
+    
 
 def main():
     #Creating socket object with IPV4 and TCP protocol
@@ -45,7 +76,6 @@ def main():
             client, address = server.accept()  
             print(f'Successfully connected to {address[0]} : {address[1]}')
             threading.Thread(target=client_handler, args=(client, )).start()
-
     except:
         print("Bind Unsuccessful!")
 
